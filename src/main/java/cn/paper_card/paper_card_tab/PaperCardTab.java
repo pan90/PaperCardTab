7854d2f9694cdc6238f8165395bd389f840629d3
Paper99 <1398872296@qq.com>
2023-09-29 23:09:42 +0800

    显示MSPT和TPS,平均Ping

--- src/main/java/cn/paper_card/paper_card_tab/PaperCardTab.java
+++ src/main/java/cn/paper_card/paper_card_tab/PaperCardTab.java
@@ -1,7 +1,7 @@
 package cn.paper_card.paper_card_tab;
 
-import com.destroystokyo.paper.event.server.ServerTickEndEvent;
-import com.destroystokyo.paper.event.server.ServerTickStartEvent;
+import io.papermc.paper.threadedregions.TickData;
+import io.papermc.paper.threadedregions.TickRegionScheduler;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
@@ -18,50 +18,108 @@ import java.util.concurrent.TimeUnit;
 
 public final class PaperCardTab extends JavaPlugin implements Listener {
 
-    private long startTime = -1;
-    private long totalTime = 0;
-    private int ticks = 0;
-
-    private double mspt = 0;
-
     private void updateTab() {
         final TextComponent header = Component.text()
-                .append(Component.text("Paper Card Server").color(NamedTextColor.RED)
+                .append(Component.text("纸 ").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD).decorate(TextDecoration.ITALIC))
+                .append(Component.text("Paper Card").color(NamedTextColor.RED)
                         .decorate(TextDecoration.BOLD))
+                .append(Component.space())
+                .append(Component.text(" 片").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD).decorate(TextDecoration.ITALIC))
                 .append(Component.newline())
                 .build();
 
+
+        // 计算平均Ping
+        long totalPing = 0;
+        int c = 0;
+        for (final Player player : this.getServer().getOnlinePlayers()) {
+            totalPing += player.getPing();
+            ++c;
+        }
+        final double avgPing = (double) totalPing / c;
+
         final long current = System.currentTimeMillis();
 
-
         for (final Player player : this.getServer().getOnlinePlayers()) {
-            if (player == null) continue;
 
-            long dayNo = current - player.getFirstPlayed();
-            dayNo /= (1000L * 60 * 60 * 24);
-            dayNo += 1;
+            player.getScheduler().run(this, task -> {
 
-            final TextComponent footer = Component.text()
-                    .append(Component.text("Ping: %d    MSPT: %.2f".formatted(player.getPing(), this.mspt)))
-                    .append(Component.newline())
-                    .append(Component.text("在线人数: %d/%d".formatted(
-                            this.getServer().getOnlinePlayers().size(),
-                            this.getServer().getOfflinePlayers().length))
-                    )
-                    .append(Component.newline())
-                    .append(Component.text("这是你游玩服务器的第"))
-                    .append(Component.text(dayNo).color(NamedTextColor.LIGHT_PURPLE).decorate(TextDecoration.BOLD))
-                    .append(Component.text("天"))
-                    .build();
+                final TickData.TickReportData report = TickRegionScheduler.getCurrentRegion()
+                        .getData()
+                        .getRegionSchedulingHandle()
+                        .getTickReport5s(System.nanoTime());
+
+                final double mspt = report.timePerTickData().segmentAll().average() / 1.0E6;
+
+                final double tps = mspt < 50 ? 20 : 1000.0 / mspt;
+
+                final int ping = player.getPing();
 
 
-            player.sendPlayerListHeader(header);
-            player.sendPlayerListFooter(footer);
-            player.playerListName(Component.text()
-                    .append(player.displayName())
-                    .append(Component.space())
-                    .append(Component.text(player.getPing()).color(NamedTextColor.YELLOW))
-                    .build());
+                // MSPT显示颜色
+                final NamedTextColor msptColor;
+                if (mspt < 50) msptColor = NamedTextColor.GREEN;
+                else if (mspt < 55) msptColor = NamedTextColor.GOLD;
+                else msptColor = NamedTextColor.RED;
+
+                final NamedTextColor pingColor;
+                if (ping < 200) pingColor = NamedTextColor.GREEN;
+                else if (ping < 500) pingColor = NamedTextColor.GOLD;
+                else pingColor = NamedTextColor.RED;
+
+                final NamedTextColor avgPingColor;
+                if (avgPing < 200) avgPingColor = NamedTextColor.GREEN;
+                else if (avgPing < 500) avgPingColor = NamedTextColor.GOLD;
+                else avgPingColor = NamedTextColor.RED;
+
+
+                long dayNo = current - player.getFirstPlayed();
+                dayNo /= (1000L * 60 * 60 * 24);
+                dayNo += 1;
+
+                final TextComponent footer = Component.text()
+                        .append(Component.text("AvgPing: "))
+                        .append(Component.text("%.2f".formatted(avgPing)).color(avgPingColor))
+                        .append(Component.text("  "))
+                        .append(Component.text("Ping: "))
+                        .append(Component.text("%d".formatted(ping)).color(pingColor))
+                        .append(Component.text("  "))
+                        .append(Component.text("TPS: "))
+                        .append(Component.text("%.2f".formatted(tps)).color(msptColor))
+                        .append(Component.text("  "))
+                        .append(Component.text("MSPT: "))
+                        .append(Component.text("%.2f".formatted(mspt)).color(msptColor))
+                        .append(Component.newline())
+
+                        .append(Component.text(("在线: %d" +
+                                "/%d").formatted(
+                                this.getServer().getOnlinePlayers().size(),
+                                this.getServer().getOfflinePlayers().length))
+                        )
+                        .append(Component.text("    "))
+                        .append(Component.text("视距：%d  模拟：%d  发送：%d".formatted(
+                                player.getViewDistance(),
+                                player.getSimulationDistance(),
+                                player.getSendViewDistance()
+                        )))
+                        .appendNewline()
+
+                        .append(Component.text("这是你游玩服务器的第"))
+                        .append(Component.text(dayNo).color(NamedTextColor.LIGHT_PURPLE).decorate(TextDecoration.BOLD))
+                        .append(Component.text("天"))
+                        .build();
+
+                player.sendPlayerListHeader(header);
+                player.sendPlayerListFooter(footer);
+                player.playerListName(Component.text()
+                        .append(player.displayName())
+                        .append(Component.space())
+                        .append(Component.text(player.getPing()).color(NamedTextColor.YELLOW))
+                        .build());
+
+            }, null);
+
+
         }
     }
 
@@ -73,25 +131,6 @@ public final class PaperCardTab extends JavaPlugin implements Listener {
     @EventHandler
     public void onQuit(@NotNull PlayerQuitEvent event) {
         this.updateTab();
-    }
-
-    @EventHandler
-    public void on1(@NotNull ServerTickStartEvent event) {
-        this.startTime = System.currentTimeMillis();
-    }
-
-    @EventHandler
-    public void on2(@NotNull ServerTickEndEvent event) {
-
-        this.totalTime += System.currentTimeMillis() - this.startTime;
-
-        ++this.ticks;
-
-        if (this.ticks == 20) {
-            this.mspt = (double) this.totalTime / this.ticks;
-            this.ticks = 0;
-            this.totalTime = 0;
-        }
     }
 
 
